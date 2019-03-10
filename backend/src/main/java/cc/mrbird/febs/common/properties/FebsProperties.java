9225d23326454e2da08f6fb172773b03b0549c87
mrbird <852252810@qq.com>
2019-03-10 15:54:04 +0800

    添加mybatis plus代码生成器

--- backend/src/main/java/cc/mrbird/febs/common/properties/FebsProperties.java
+++ backend/src/main/java/cc/mrbird/febs/common/properties/FebsProperties.java
@@ -1,8 +1,10 @@
 package cc.mrbird.febs.common.properties;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+@Data
 @Configuration
 @ConfigurationProperties(prefix = "febs")
 public class FebsProperties {
@@ -11,19 +13,4 @@ public class FebsProperties {
 
     private boolean openAopLog = true;
 
-    public ShiroProperties getShiro() {
-        return shiro;
-    }
-
-    public void setShiro(ShiroProperties shiro) {
-        this.shiro = shiro;
-    }
-
-    public boolean isOpenAopLog() {
-        return openAopLog;
-    }
-
-    public void setOpenAopLog(boolean openAopLog) {
-        this.openAopLog = openAopLog;
-    }
 }
