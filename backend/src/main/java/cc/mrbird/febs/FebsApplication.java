fd18f9661bfac481837b12f8076d4162b4f40ae9
mrbird <852252810@qq.com>
2019-03-10 14:17:53 +0800

    启动类改为Fluent API

--- backend/src/main/java/cc/mrbird/febs/FebsApplication.java
+++ backend/src/main/java/cc/mrbird/febs/FebsApplication.java
@@ -1,19 +1,19 @@
 package cc.mrbird.febs;
 
-import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication(exclude = {DruidDataSourceAutoConfigure.class})
+@SpringBootApplication
 @EnableTransactionManagement
 @EnableScheduling
 @EnableAsync
 public class FebsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FebsApplication.class, args);
+        new SpringApplicationBuilder(FebsApplication.class)
+                .run(args);
     }
 }
