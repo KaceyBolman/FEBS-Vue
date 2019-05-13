1d537a6f70ecb3e3b89f7e9cee00e58063a2150b
kaceybolman <kaceyfaltusibed@yahoo.com>
2019-05-13 16:12:05 +0800

    修复首页显示暂无

--- backend/src/main/java/cc/mrbird/febs/system/dao/UserMapper.java
+++ backend/src/main/java/cc/mrbird/febs/system/dao/UserMapper.java
@@ -8,5 +8,13 @@ import org.apache.ibatis.annotations.Param;
 
 public interface UserMapper extends BaseMapper<User> {
 
-	IPage<User> findUserDetail(Page page, @Param("user") User user);
+    IPage<User> findUserDetail(Page page, @Param("user") User user);
+
+    /**
+     * 获取单个用户详情
+     *
+     * @param username 用户名
+     * @return 用户信息
+     */
+    User findDetail(String username);
 }
