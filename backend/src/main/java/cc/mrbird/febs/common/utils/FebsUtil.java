e89e305e62c8d723562ed85933e6b68c9a3e1f2a
kaceybolman <kaceyfaltusibed@yahoo.com>
2019-05-20 10:38:15 +0800

    优化缓存查询模板方法

--- backend/src/main/java/cc/mrbird/febs/common/utils/FebsUtil.java
+++ backend/src/main/java/cc/mrbird/febs/common/utils/FebsUtil.java
@@ -21,23 +21,28 @@ import java.util.stream.IntStream;
 public class FebsUtil {
 
     /**
-     * 缓存查询摸板，先查缓存，如果缓存查询失败再从数据库查询
+     * 缓存查询模板
      *
      * @param cacheSelector    查询缓存的方法
      * @param databaseSelector 数据库查询方法
      * @return T
      */
-    @SuppressWarnings("unchecked")
-    public static <T> T selectCacheByTemplate(CacheSelector<?> cacheSelector, Supplier<?> databaseSelector) {
+    public static <T> T selectCacheByTemplate(CacheSelector<T> cacheSelector, Supplier<T> databaseSelector) {
         try {
-            // 先查 Redis缓存
             log.debug("query data from redis ······");
-            return (T) cacheSelector.select();
+            // 先查 Redis缓存
+            T t = cacheSelector.select();
+            if (t == null) {
+                // 没有记录再查询数据库
+                return databaseSelector.get();
+            } else {
+                return t;
+            }
         } catch (Exception e) {
-            // 数据库查询
+            // 缓存查询出错，则去数据库查询
             log.error("redis error：", e);
             log.debug("query data from database ······");
-            return (T) databaseSelector.get();
+            return databaseSelector.get();
         }
     }
 
