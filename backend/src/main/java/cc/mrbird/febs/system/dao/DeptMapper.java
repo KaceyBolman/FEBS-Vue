333d4a640cd8daebc514e859b5d3c7dde20b1092
kaceybolman <kaceyfaltusibed@yahoo.com>
2019-08-01 15:35:30 +0800

    删除数据库自定义函数，优化group_by

--- backend/src/main/java/cc/mrbird/febs/system/dao/DeptMapper.java
+++ backend/src/main/java/cc/mrbird/febs/system/dao/DeptMapper.java
@@ -5,10 +5,4 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
 public interface DeptMapper extends BaseMapper<Dept> {
 
-	/**
-	 * 递归删除部门
-	 *
-	 * @param deptId deptId
-	 */
-	void deleteDepts(String deptId);
 }
